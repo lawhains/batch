7d1190b1478dc66019819ac74a7a9690ee0683b1
hainswl <hainswl@wwu.edu>
2026-02-23 20:07:51 -0800

    initial project setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Batch API running'));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port 3000');
+});
